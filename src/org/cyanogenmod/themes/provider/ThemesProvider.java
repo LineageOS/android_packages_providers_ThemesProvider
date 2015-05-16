6f96dba085685bececaff14344fadd26bb931e41
Richard MacGregor <rmacgregor@cyngn.com>
2015-05-16 19:48:34 +0000

    Revert "[1/2] Use files instead of blobs"
    This reverts commit 8fd765a1646bdbba1852d75516894c667d6b5249.
    
    Change-Id: If5a6cd32c45af69770756b49bb56a581844ca32a

--- src/org/cyanogenmod/themes/provider/ThemesProvider.java
+++ src/org/cyanogenmod/themes/provider/ThemesProvider.java
@@ -47,7 +47,6 @@ import org.cyanogenmod.themes.provider.ThemesOpenHelper.PreviewsTable;
 import org.cyanogenmod.themes.provider.ThemesOpenHelper.ThemesTable;
 import org.cyanogenmod.themes.provider.util.ProviderUtils;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -108,23 +107,13 @@ public class ThemesProvider extends ContentProvider {
             SQLiteDatabase sqlDB = mDatabase.getWritableDatabase();
 
             // Get the theme's _id and delete preview images
-            int idx = -1;
-            String[] columns = new String[] { ThemesColumns._ID, ThemesColumns.PKG_NAME };
+            String[] columns = new String[] { ThemesColumns._ID };
             Cursor c = sqlDB.query(ThemesTable.TABLE_NAME, columns, selection,
                     selectionArgs, null, null, null);
             if (c == null) return 0;
             if (c.moveToFirst()) {
-                idx = c.getColumnIndex(ThemesColumns._ID);
                 sqlDB.delete(PreviewsTable.TABLE_NAME,
-                        PreviewColumns.THEME_ID + "=" + c.getInt(idx), null);
-
-                // Remove preview files associated with theme
-                idx = c.getColumnIndex(ThemesColumns.PKG_NAME);
-                String pkgName = c.getString(idx);
-                String filesDir = getContext().getFilesDir().getAbsolutePath();
-                String themePreviewsDir = filesDir + File.separator +
-                        PreviewGenerationService.PREVIEWS_DIR + File.separator + pkgName;
-                PreviewGenerationService.clearThemePreviewsDir(themePreviewsDir);
+                        PreviewColumns.THEME_ID + "=" + c.getInt(0), null);
             }
             c.close();
 
